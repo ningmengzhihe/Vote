8ff4163e9415cb7f500814551cef67ebfa42f3d1
李晗 <1539647933@qq.com>
2022-10-11 09:21:39 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# 教程
+https://blog.csdn.net/weixin_45629315/article/details/113469323
+
+# 笔记和注释
+https://blog.csdn.net/ningmengzhihe/article/details/127048200
+
+# 源代码说明
+本地代码只需要app.js和index.html
+
+V.sol文件是存放在在线代码编辑器Remix上
+
+# 运行方法
+不需要任何依赖，用浏览器打开index.html即可看到可视化界面
---
+++ app.js
@@ -0,0 +1,29 @@
+web3 = new Web3(new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545"));
+abi = JSON.parse('[{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+VotingContract = web3.eth.contract(abi);
+
+//部署的合约地址
+contractInstance = VotingContract.at('0x184AbCC8D9A66EC572B7C7236e23C48db96a27D0');
+
+candidates = {"Alice": "candidate-1","Bob": "candidate-2","Cary":"candidate-3"}
+
+function voteForCandidate() {
+    console.log(candidate);
+    candidateName = $("#candidate").val();
+    console.log(candidateName);
+    contractInstance.voteForCandidate(candidateName, {from: web3.eth.accounts[0]}, function() {
+        let div_id = candidates[candidateName];
+        console.log(contractInstance.totalVotesFor.call(candidateName).toString());
+        $("#" + div_id).html(contractInstance.totalVotesFor.call(candidateName).toString());
+    });
+    console.log(contractInstance.totalVotesFor.call(candidateName).toString());
+}
+
+$(document).ready(function() {
+    candidateNames = Object.keys(candidates);
+    for (var i = 0; i < candidateNames.length; i++) {
+        let name = candidateNames[i];
+        let val = contractInstance.totalVotesFor.call(name).toString()
+        $("#" + candidates[name]).html(val);
+    }
+});
